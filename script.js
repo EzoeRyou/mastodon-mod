8f3137f941335b405b00a47d2174d70630e14b92
Ryou Ezoe <boostcpp@gmail.com>
2017-04-26 17:23:07 +0900

    fix

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+var config = document.createElement( "script" ) ;
+config.type = "text/x-mathjax-config" ;
+config.appendChild( document.createTextNode( "MathJax.Hub.Config({tex2jax: {inlineMath: [ ['$','$'], ['\\\\(', '\\\\)'] ] }});" ) ) ;
+
+document.head.appendChild(config) ;
+
 var script = document.createElement( "script" ) ;
 script.type = "text/javascript" ;
 script.async="async" ;
-script.src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-MML-AM_CHTML" ;
-script.appendChild( document.createTextNode('window.setInterval( function() { MathJax.Hub.Queue(["Typeset",MathJax.Hub]); }, 1000 ) ;') ) ;
+script.src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG" ;
 
+script.appendChild( document.createTextNode('window.setInterval( function() { MathJax.Hub.Queue(["Typeset",MathJax.Hub]); }, 1000 ) ;') ) ;
 
 document.head.appendChild(script) ;
 
